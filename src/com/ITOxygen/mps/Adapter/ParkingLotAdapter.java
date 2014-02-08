7693ba6a56cd95b70d09213d95d882a467912fb6
eacarter <eacarter@mtu.edu>
2014-02-08 14:16:07 -0500

    setfiles

--- src/com/ITOxygen/mps/Adapter/ParkingLotAdapter.java
+++ src/com/ITOxygen/mps/Adapter/ParkingLotAdapter.java
@@ -1,5 +1,23 @@
 package com.ITOxygen.mps.Adapter;
 
-public class ParkingLotAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ITOxygen.MPSModels.ParkingModel;
+import com.haarman.listviewanimations.itemmanipulation.ExpandableListItemAdapter;
+
+public class ParkingLotAdapter extends ExpandableListItemAdapter<ParkingModel>{
+
+	@Override
+	public View getContentView(int arg0, View arg1, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public View getTitleView(int arg0, View arg1, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
