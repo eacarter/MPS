50714a644ef9d60a3c55ae5cddbf830aa96545bd
eacarter <eacarter@mtu.edu>
2014-02-15 14:01:51 -0500

    Added Parse and Array Adapter

--- src/com/ITOxygen/mps/Adapter/ParkingLotAdapter.java
+++ src/com/ITOxygen/mps/Adapter/ParkingLotAdapter.java
@@ -1,12 +1,20 @@
 package com.ITOxygen.mps.Adapter;
 
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ITOxygen.MPSModels.ParkingModel;
+import com.ITOxygen.mps.R;
 import com.haarman.listviewanimations.itemmanipulation.ExpandableListItemAdapter;
 
 public class ParkingLotAdapter extends ExpandableListItemAdapter<ParkingModel>{
+
+	private Context mContext; 
 
 	@Override
 	public View getContentView(int arg0, View arg1, ViewGroup arg2) {
@@ -15,9 +23,25 @@ public class ParkingLotAdapter extends ExpandableListItemAdapter<ParkingModel>{
 	}
 
 	@Override
-	public View getTitleView(int arg0, View arg1, ViewGroup arg2) {
-		// TODO Auto-generated method stub
-		return null;
+	public View getTitleView(int position, View convertView, ViewGroup parent) {
+		View v = convertView;
+		
+		if(v == null){
+			v = LayoutInflater.from(mContext).inflate(R.layout.parkingtitle, parent, false);
+		}
+		
+		TextView title = (TextView) v.findViewById(R.id.title);
+		TextView state = (TextView) v.findViewById(R.id.state);
+		
+		title.setText("lot " + getItem(position).getNumber());
+		state.setText(getItem(position).getState());
+
+		return v;
 	}
 
+	public ParkingLotAdapter(Context context, List<ParkingModel> items ){
+		super(context, R.layout.activtiy_expandablelistitem_card, R.id.activity_expandablelistitem_card_title, R.id.activity_expandablelistitem_card_content, items);
+		mContext =context;
+
+	}
 }
--- src/com/ITOxygen/mps/MainActivity.java
+++ src/com/ITOxygen/mps/MainActivity.java
@@ -1,14 +1,22 @@
 package com.ITOxygen.mps;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.ITOxygen.MPSModels.ParkingModel;
+import com.ITOxygen.mps.Adapter.ParkingLotAdapter;
+import com.haarman.listviewanimations.swinginadapters.prepared.SwingBottomInAnimationAdapter;
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ListView;
+import android.view.LayoutInflater;
 
 public class MainActivity extends Activity {
 
@@ -16,7 +24,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		ListView List = (ListView) R.layout.findViewById(R.id.ParkingLotList);
+		
+		ListView List = (ListView) findViewById(R.id.ParkingLotList);
 		
 		Parse.initialize(getApplication(), DeveloperKey.AppID, DeveloperKey.ClientKey);
 		
@@ -24,12 +33,33 @@ public class MainActivity extends Activity {
 		
 		ArrayList<ParkingModel> modelList = populate();
 		
+		ParkingLotAdapter pAdapter = new ParkingLotAdapter(this, modelList);
 		
+		SwingBottomInAnimationAdapter animation = new SwingBottomInAnimationAdapter(pAdapter);
+		
+		animation.setAbsListView(List);
+		animation.setInitialDelayMillis(500);
+		
+		List.setAdapter(animation);
 	}
 
+	/*
+	 * 
+	 */
 	private ArrayList<ParkingModel> populate() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<ParkingModel> modelList = new ArrayList<ParkingModel>();
+		ParseQuery<ParseObject> parkingQuery = ParseQuery.getQuery("ParkingLots");
+		parkingQuery.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE);
+		List<ParseObject> list;
+		try {
+			list = parkingQuery.find();
+			for(ParseObject po : list){
+				modelList.add(new ParkingModel(po));
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return modelList;
 	}
 
 	@Override
